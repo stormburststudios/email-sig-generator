fdc0bb08903c9399cd3680b5aa2f1640185d6af2
Alex Wiley <alex.wiley@oneupsales.co.uk>
2023-03-06 21:24:09 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,215 @@
+import { useState, useRef } from 'react';
+import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { EmailTemplate } from './components/EmailTemplate/EmailTemplate';
+
+function App() {
+  const [image, setImage] = useState(null);
+  const [forename, setForename] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [position, setPosition] = useState('');
+  const [linkedIn, setLinkedIn] = useState('');
+  const [mobile, setMobile] = useState('');
+  const template = useRef(null);
+
+  const copy = () => {
+    const range = document.createRange();
+    range.selectNodeContents(template.current);
+    const sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+    document.execCommand('copy');
+    toast('Copied HTML', { type: toast.TYPE.SUCCESS });
+  }
+
+  const reset = () => {
+    setImage(null);
+    setForename('');
+    setSurname('');
+    setEmail('');
+    setPosition('');
+    setLinkedIn('');
+    setMobile('');
+  }
+
+  return (
+      <div style={{padding: '50px 100px 100px 100px', backgroundColor: '#EFF3F9' }}>
+        <ToastContainer />
+        <div className="mt-5 md:col-span-2 md:mt-0" style={{ marginBottom: '30px' }}>
+          <div className="shadow sm:overflow-hidden sm:rounded-md">
+            <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+              <p className="text-lg font-bold" style={{ margin: 0 }}>Preview</p>
+              <div ref={template}>
+                <EmailTemplate
+                    picture={image}
+                    forename={forename}
+                    surname={surname}
+                    email={email}
+                    position={position}
+                    linkedIn={linkedIn}
+                    mobile={mobile}
+                />
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+              <a href="https://www.loom.com/share/586c20b4d4084787861356246ff1ff52" target="_blank" style={{ textDecoration: 'none' }}>
+                <button
+                    type="submit"
+                    className="inline-flex justify-center rounded-md border border-transparent text-black bg-slate-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-slate-500 focus:outline-none focus:ring-2 focus:bg-slate-400 focus:ring-offset-2"
+                    style={{ marginRight: '10px' }}
+                >
+                  Learn How To Set In Gmail
+                </button>
+              </a>
+              <button
+                  type="submit"
+                  className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  onClick={copy}
+              >
+                Copy HTML
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <div className="shadow sm:overflow-hidden sm:rounded-md">
+            <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-6">
+                  <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                    Image URL
+                  </label>
+                  <input
+                      type="text"
+                      name="url"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="field mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      placeholder="https://ca.slack-edge.com/T3A03KQ6S-UK21Z3L2X-48206164cf0e-512"
+                      value={image}
+                      onChange={({target}) => setImage(target.value)}
+                  />
+                  <a className="help" href="https://www.loom.com/share/0af2cce327854f9aaf9413562afcc747" target="_blank">Learn how to get your image URL</a>
+                  <a className="help" href="https://imgur.com" target="_blank">Go to imgur.com</a>
+                </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                    First name
+                  </label>
+                  <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="field mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      placeholder="John"
+                      value={forename}
+                      onChange={({target}) => setForename(target.value)}
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                    Last name
+                  </label>
+                  <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="field mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      placeholder="Doe"
+                      value={surname}
+                      onChange={({target}) => setSurname(target.value)}
+                  />
+                </div>
+
+                <div className="col-span-3">
+                  <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
+                    Position
+                  </label>
+                  <input
+                      type="text"
+                      name="position"
+                      id="position"
+                      className="field mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      placeholder="Head of Doe"
+                      value={position}
+                      onChange={({target}) => setPosition(target.value)}
+                  />
+                </div>
+
+                <div className="col-span-3">
+                  <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+                    Email address
+                  </label>
+                  <input
+                      type="text"
+                      name="email-address"
+                      id="email-address"
+                      autoComplete="email"
+                      className="field mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      placeholder="john.doe@oneupsales.co.uk"
+                      value={email}
+                      onChange={({target}) => setEmail(target.value)}
+                  />
+                </div>
+
+
+                <div className="col-span-6 sm:col-span-6 lg:col-span-3">
+                  <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+                    LinkedIn URL
+                  </label>
+                  <input
+                      type="text"
+                      name="ln"
+                      id="ln"
+                      className="field mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      placeholder="https://www.linkedin.com/in/johndoe"
+                      value={linkedIn}
+                      onChange={({target}) => setLinkedIn(target.value)}
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-2 lg:col-span-3">
+                  <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
+                    Phone
+                  </label>
+                  <div className="mt-1 flex rounded-md shadow-sm">
+                        <span style={{backgroundColor: '#e5e5e5' }} className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
+                          +44
+                        </span>
+                    <input
+                        type="number"
+                        name="mobile"
+                        id="mobile"
+                        className="field block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        placeholder="7947563882"
+                        autoComplete="tel-local"
+                        value={mobile}
+                        onChange={({target}) => setMobile(target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+              <button
+                  type="submit"
+                  className="inline-flex justify-center rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                  onClick={reset}
+              >
+                Reset
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/EmailTemplate/EmailTemplate.js
@@ -0,0 +1,204 @@
+export const EmailTemplate = ({
+        picture = 'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
+        forename = 'John',
+        surname = 'Doe',
+        email = 'john.doe@oneupsales.co.uk',
+        position = 'Head of Doe',
+        linkedIn = 'https://linkedin.com',
+        mobile = '798 574 3223',
+    }) => {
+        const fallback = (str, fallback) => str === null ? fallback : str.length === 0 ? fallback : str;
+        return (
+            <div className="template">
+                <table cellPadding={0} cellSpacing={0}
+                       style={{ verticalAlign: '-webkit-baseline-middle', fontFamily: 'Verdana' }}>
+                    <tbody>
+                    <tr>
+                        <td style={{ verticalAlign: 'top' }}>
+                            <table cellPadding={0} cellSpacing={0}
+                                   style={{ verticalAlign: '-webkit-baseline-middle', fontFamily: 'Verdana' }}>
+                                <tbody>
+                                <tr>
+                                    <td style={{ textAlign: 'center' }}>
+                                        <img width={110} style={{
+                                            width: '110px',
+                                            height: '110px',
+                                            display: 'block',
+                                            borderRadius: '50%',
+                                            objectFit: 'cover',
+                                        }}
+                                             src={fallback(picture, 'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80')}
+                                             className="CToWUd" data-bit="iit"/>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td width={30}>
+                            <div/>
+                        </td>
+                        <td style={{ padding: '0px', verticalAlign: 'middle' }}>
+                            <h3 color="#000000" style={{
+                                color: 'rgb(0, 0, 0)',
+                                fontFamily: 'Verdana',
+                                margin: '0px',
+                                fontSize: '18px',
+                            }}>{fallback(forename, 'John')} {fallback(surname, 'Doe')}</h3>
+                            <p color="#000000" style={{
+                                color: 'rgb(0, 0, 0)',
+                                fontFamily: 'Verdana',
+                                margin: '0px',
+                                fontSize: '14px',
+                                lineHeight: '22px',
+                                opacity: '0.8',
+                            }}>{fallback(position, 'Head of Doe')}</p>
+                            <img width={110} style={{ maxWidth: '110px', display: 'block', marginTop: '10px' }}
+                                 src="https://ci3.googleusercontent.com/mail-sig/AIorK4yNXlLuKoIapFdlTYvFLVvXH2fc37nvaLwrBZnOAAxum0fjnydP8cIqSbcYx2OU1GHyT3DYZCU"
+                                 className="CToWUd" data-bit="iit"/>
+                            <table cellPadding={0} cellSpacing={0} style={{
+                                fontFamily: 'Verdana',
+                                verticalAlign: '-webkit-baseline-middle',
+                                width: '221px',
+                            }}>
+                                <tbody>
+                                <tr>
+                                    <td height={15}/>
+                                </tr>
+                                <tr>
+                                    <td color="#2489FD" height={1} style={{
+                                        width: '221px',
+                                        borderBottom: '2px solid #2489fd',
+                                        borderLeft: 'none',
+                                        display: 'block',
+                                    }}/>
+                                </tr>
+                                <tr>
+                                    <td height={15}/>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <table cellPadding={0} cellSpacing={0}
+                                   style={{ fontFamily: 'Verdana', verticalAlign: '-webkit-baseline-middle' }}>
+                                <tbody>
+                                <tr height={25} style={{ verticalAlign: 'middle' }}>
+                                    <td width={30} style={{ verticalAlign: 'middle' }}>
+                                        <table cellPadding={0} cellSpacing={0}
+                                               style={{ verticalAlign: '-webkit-baseline-middle', fontFamily: 'Verdana' }}>
+                                            <tbody>
+                                            <tr>
+                                                <td style={{ verticalAlign: 'bottom' }}>
+                                                    <span color="#F2547D" width={11} style={{ display: 'block' }}><img
+                                                        src="https://i.ibb.co/T8RhHWh/phone-solid.png" width={13}
+                                                        style={{ display: 'block' }} className="CToWUd"
+                                                        data-bit="iit"/></span>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                    <td style={{ padding: '0px', color: 'rgb(0, 0, 0)' }}><a href={`tel:${fallback(mobile, '798 574 3223')}`}
+                                                                                             color="#000000" style={{
+                                        color: 'rgb(0, 0, 0)',
+                                        fontSize: '12px',
+                                        textDecoration: 'none',
+                                    }} target="_blank" rel="noreferrer">+44 {fallback(mobile, '798 574 3223')}</a></td>
+                                </tr>
+                                <tr height={25} style={{ verticalAlign: 'middle' }}>
+                                    <td width={30} style={{ verticalAlign: 'middle' }}>
+                                        <table cellPadding={0} cellSpacing={0}
+                                               style={{ verticalAlign: '-webkit-baseline-middle', fontFamily: 'Verdana' }}>
+                                            <tbody>
+                                            <tr>
+                                                <td style={{ verticalAlign: 'bottom' }}>
+                                  <span color="#F2547D" width={11} style={{ display: 'block' }}>
+                                    <img src="https://i.ibb.co/k07jLSm/envelope-solid.png" color="#F2547D" width={13}
+                                         style={{ display: 'block' }} className="CToWUd" data-bit="iit"/>
+                                  </span>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                    <td style={{ padding: '0px' }}>
+                                        <a href={`mailto:${fallback(email, 'john.doe@example.com')}`} color="#000000"
+                                           style={{ color: 'rgb(0, 0, 0)', fontSize: '12px', textDecoration: 'none' }}
+                                           target="_blank" rel="noreferrer">
+                                            <span className="il" style={{}}>{fallback(email, 'john.doe@example.com')}</span>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr height={25} style={{ verticalAlign: 'middle' }}>
+                                    <td width={30} style={{ verticalAlign: 'middle' }}>
+                                        <table cellPadding={0} cellSpacing={0}
+                                               style={{ verticalAlign: '-webkit-baseline-middle', fontFamily: 'Verdana' }}>
+                                            <tbody>
+                                            <tr>
+                                                <td style={{ verticalAlign: 'bottom' }}>
+                                  <span color="#F2547D" width="15px" style={{ display: 'block', width: '11px' }}>
+                                    <img src="https://i.ibb.co/StF13TY/globe-solid.png" color="#F2547D" width={13}
+                                         style={{ display: 'block' }} className="CToWUd" data-bit="iit"/>
+                                  </span>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                    <td style={{ padding: '0px' }}>
+                                        <a href="https://www.oneupsales.co.uk/" color="#000000"
+                                           style={{ color: 'rgb(0, 0, 0)', fontSize: '12px', textDecoration: 'none' }}
+                                           target="_blank"
+                                           data-saferedirecturl="https://www.google.com/url?q=https://www.oneupsales.co.uk/&source=gmail&ust=1676503559876000&usg=AOvVaw1mMgFIw9EyN1A6fkJepWaw" rel="noreferrer">
+                                            https://www.oneupsales.co.uk/
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr height={25} style={{ verticalAlign: 'middle' }}>
+                                    <td width={30} style={{ verticalAlign: 'middle' }}>
+                                        <table cellPadding={0} cellSpacing={0}
+                                               style={{ verticalAlign: '-webkit-baseline-middle', fontFamily: 'Verdana' }}>
+                                            <tbody>
+                                            <tr>
+                                                <td style={{ verticalAlign: 'bottom' }}>
+                                  <span color="#F2547D" width="15px" style={{ display: 'block', width: '11px' }}>
+                                    <img src="https://i.ibb.co/H4bxv79/linkedin.png" color="#F2547D" width={13}
+                                         style={{ display: 'block' }} className="CToWUd" data-bit="iit"/>
+                                  </span>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                    <td style={{ padding: '0px' }}>
+                                        <a href={fallback(linkedIn, 'https://linkedin.com')} color="#000000"
+                                           style={{ color: 'rgb(0, 0, 0)', fontSize: '12px', textDecoration: 'none' }}
+                                           target="_blank"
+                                           rel="noreferrer">
+                                            Connect with me on LinkedIn
+                                        </a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <table cellPadding={0} cellSpacing={0}
+                                   style={{ fontFamily: 'Verdana', verticalAlign: '-webkit-baseline-middle' }}>
+                                <tbody>
+                                <tr>
+                                    <td height={20}/>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <br/>
+                            <table cellPadding={0} cellSpacing={0}
+                                   style={{ fontFamily: 'Verdana', verticalAlign: '-webkit-baseline-middle' }}>
+                                <tbody>
+                                <tr/>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+;
